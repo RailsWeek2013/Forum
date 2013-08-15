56f1358753ea8fca4786b8a169f8f93ae94eef42
Moritz Muench <moritz.muench@mni.thm.de>
2013-08-15 23:23:16 +0200

    Links korrigiert und Hilfsvariablen hinzugefuegt

--- app/controllers/user_threads_controller.rb
+++ app/controllers/user_threads_controller.rb
@@ -1,9 +1,10 @@
 class UserThreadsController < ApplicationController
+  before_action :set_user_thread_topic
   before_action :set_user_thread, only: [:show, :edit, :update, :destroy]
 
   # GET /user_threads
   def index
-    @user_threads = Topic.find(params[:topic_id]).userThreads.all
+    @user_threads = @topic.userThreads.all
   end
 
   # GET /user_threads/1
@@ -12,7 +13,7 @@ class UserThreadsController < ApplicationController
 
   # GET /user_threads/new
   def new
-    @user_thread = Topic.find(params[:topic_id]).userThreads.new
+    @user_thread = @topic.userThreads.new
   end
 
   # GET /user_threads/1/edit
@@ -21,11 +22,11 @@ class UserThreadsController < ApplicationController
 
   # POST /user_threads
   def create
-      @user_thread = UserThread.new(user_thread_params)
-      @user_thread.topic = Topic.find(params[:topic_id])
+      @user_thread = @topic.userThreads.new(user_thread_params)
+      #@user_thread.topic = Topic.find(params[:topic_id]) # sollte er selber setzen
 
       if @user_thread.save
-        redirect_to [@user_thread.topic, @user_thread], notice: 'User thread was successfully created.'
+        redirect_to [@topic, @user_thread], notice: 'User thread was successfully created.'
       else
         render action: 'new'
       end
@@ -34,7 +35,7 @@ class UserThreadsController < ApplicationController
   # PATCH/PUT /user_threads/1
   def update
       if @user_thread.update(user_thread_params)
-        redirect_to [@user_thread.topic, @user_thread], notice: 'User thread was successfully updated.'
+        redirect_to [@topic, @user_thread], notice: 'User thread was successfully updated.'
       else
         render action: 'edit'
       end
@@ -47,9 +48,14 @@ class UserThreadsController < ApplicationController
   end
 
   private
+
+    def set_user_thread_topic
+      @topic = Topic.find(params[:topic_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_user_thread
-      @user_thread = Topic.find(params[:topic_id]).userThreads.find(params[:id])
+      @user_thread = @topic.userThreads.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
