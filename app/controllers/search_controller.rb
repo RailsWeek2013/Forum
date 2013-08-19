62824619624f99218271f2e752fea221dda29ca3
Phillip Trusheim <phillip.trusheim@mni.thm.de>
2013-08-19 13:22:56 +0200

    einfache Suchfunktion  eingebaut

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,12 @@
+class SearchController < ApplicationController
+	before_action :setPosts
+  def search
+  	@results =  @posts.where("title like ?","%"+params[:search_field]+"%")
+  	@contentResults = @posts.where("content like ?","%"+params[:search_field]+"%")
+  end
+
+  private 
+  def setPosts
+  	@posts = Post.all
+  end
+end
