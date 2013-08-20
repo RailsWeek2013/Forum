764f0fe1bb31099b54eac2fa5f841de4eec9740c
Moritz Muench <moritz.muench@mni.thm.de>
2013-08-20 14:14:33 +0200

    getPage helper geaendert, link zum neusten Post eines UserThreads korrigiert

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,7 +1,7 @@
 module SearchHelper
 
   def getPage n
-    page = @posts.where("id <= #{n.id}").all.count
+    page = n.user_thread.posts.where("id <= #{n.id}").all.count
     (page/10.0).ceil
   end
 
