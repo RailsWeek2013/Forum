4faff2be42b237c14158531a77bff89f3640b24a
Phillip Trusheim <phillip_trusheim@web.de>
2013-08-24 22:17:45 +0200

    Suche findet Threads und Topics

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
 	before_action :setPosts
 	before_action :setUsers
+	before_action :setThreads
+	before_action :setTopics
 
   def search
   	@results =  @posts.where("title like ?","%"+params[:search_field]+"%")
   	@contentResults = @posts.where("content like ?","%"+params[:search_field]+"%")
   	@userResults = @users.where("email like ?","%"+params[:search_field]+"%")
+  	@threadResults = @threads.where("title like ?","%"+params[:search_field]+"%")
+  	@topicResults = @topics.where("name like ?","%"+params[:search_field]+"%")
   end
 
   private 
@@ -16,5 +20,13 @@ class SearchController < ApplicationController
 
   def setUsers
   	@users = User.all
+  end
+
+  def setThreads
+  	@threads = UserThread.all
+  end
+
+  def setTopics
+  	@topics = Topic.all
   end
 end
