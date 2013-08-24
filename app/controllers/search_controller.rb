84c4485d6efd9d63de04e067347faa3895a0fb4b
Phillip Trusheim <phillip_trusheim@web.de>
2013-08-24 22:06:52 +0200

    Suche findet jetzt User(Email)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
 class SearchController < ApplicationController
 	before_action :setPosts
+	before_action :setUsers
 
   def search
   	@results =  @posts.where("title like ?","%"+params[:search_field]+"%")
   	@contentResults = @posts.where("content like ?","%"+params[:search_field]+"%")
+  	@userResults = @users.where("email like ?","%"+params[:search_field]+"%")
   end
 
   private 
 
   def setPosts
   	@posts = Post.all
+  end
+
+  def setUsers
+  	@users = User.all
   end
 end
