f0b789c4e8a0bb8e126e2007b976873b1e536ff1
Moritz Münch <moritz.muench@mni.thm.de>
2013-12-25 23:35:21 +0100

    Helper zum erstellen eines Bootstrap panels

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -5,4 +5,15 @@ module SearchHelper
     (page/10.0).ceil
   end
 
+  def bootstrap_panel(title, &block)
+
+      content_tag :div, class: 'panel panel-default' do
+        content_tag :div, class: 'panel-body' do
+          content_tag(:h2, title.to_s.html_safe) +
+          content_tag(:div) { yield }
+        end
+      end
+
+  end
+
 end
